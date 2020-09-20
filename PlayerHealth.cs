c824400512f0b4b1cabf86c1554bbee4dc79e627
devELIOper <lopeselio@gmail.com>
2020-09-20 14:25:09 +0530

    handling death of player

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -11,7 +11,8 @@ public class PlayerHealth : MonoBehaviour
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
-            Debug.Log("You dead, my glip glop");
+            // Debug.Log("You dead, my glip glop");
+            GetComponent<DeathHandler>().HandleDeath();
         }
     }
 }
